41ee7f733757abf6cebdd667eab5c80296b84512
KGlablocal <KGlabtudelft@gmail.com>
2025-10-27 14:06:59 +0100

    commit before evaluating RI to dry-mass vs direct drymass caluclation in single channel

--- tpr.py
+++ tpr.py
@@ -7,6 +7,10 @@ import math
 
 class TopographicPhaseRetrieval: 
 
+    def __init__(self):
+
+        return 
+
     def linmap(self, val=None,valMin=None,valMax=None,mapMin=None,mapMax=None,*args):
 
         if len(*args) == 3:
@@ -76,7 +80,7 @@ class TopographicPhaseRetrieval:
                 stack=self.cropXY(stack)
 
         #phase_structure.summarise(s)
-        phase = iterate_getQP(stack, Nt, s)
+        phase = self.iterate_getQP(stack, Nt, s)
         return phase
  
     #@jit(nopython=True)   
@@ -157,7 +161,7 @@ class TopographicPhaseRetrieval:
         stack = stack[ay:(temp_nPix - 1 + ay),ax:(temp_nPix + ax - 1),:]
         return stack
 
-    def cropXY(self, input_=None,N=None):
+    def cropXY(sefl, input_=None,N=None):
         print("Cropping..")
         
         size = input_.shape
@@ -277,3 +281,41 @@ class TopographicPhaseRetrieval:
         QP = np.angle(csd + np.mean(np.ravel(stack)) / struct.optics_alpha)
 
         return QP,mask
+    
+
+
+class phase_structure: 
+    
+    def __init__(self):
+        self.optics_dx = 0.108
+        self.optics_dz = 0.1          # axial sampling [um]
+        self.optics_NA = 1.3          # Numerical Aperture detection
+        self.optics_NA_ill = 0.55     # Numerical Aperture illumination
+        self.optics_n = 1.406          # refractive index
+        self.optics_wv = 0.58          # Central wavelength in vacuum/ WAS optics.lambda (python internal command)
+        self.optics_dlambda = 0.075   # Spectrum bandwidth
+        self.optics_alpha = 4.21      # Experimental coefficient for QP "normalisation" optimised for TUD/MCL setup
+        self.optics_kzT = 0.01        # Axial cutoff frequency.
+                                        # if set to [], use the theoretical value
+    ### Practical note : Inacurate values of illumination NA and source spectrum
+    # in conjunction with coarse z-sampling may lead to an overestimation of
+    # the axial cutoff if the theoretical value is used.
+    # Best practice is to use a small value > 0
+    
+        # processing parameters
+        self.proc_mirrorX = False            # mirror the input stack along X 
+        self.proc_mirrorZ = True             # mirror the input stack along Z
+        self.proc_applyFourierMask = True    # apply the denoising Fourier mask
+                                    
+                           
+    def summarise(self):
+        print("Phase structure: \n_________________")
+        print(f"s.optics_dx = {self.optics_dx} \t \t s.optics_wv = {self.optics_wv}")
+        print(f"s.optics_dz = {self.optics_dz} \t \t s.optics_dlambda = {self.optics_dlambda}")
+        print(f"s.optics_NA = {self.optics_NA} \t \t s.optics_alpha = {self.optics_alpha}")
+        print(f"s.optics_NA_ill = {self.optics_NA_ill} \t \t s.optics_kzT = {self.optics_kzT}")
+        print(f"s.optics_n = {self.optics_n}")
+        print("Processing paramters: \n_________________")
+        print(f"s.proc_mirrorX = {self.proc_mirrorX} \t \t s.proc_mirrorZ = {self.proc_mirrorZ}")
+        print(f"s.proc_applyFourierMask = {self.proc_applyFourierMask}")
+        
